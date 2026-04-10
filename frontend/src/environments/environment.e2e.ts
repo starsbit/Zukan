9abcf83f26cd6cf5604c9290563f0077d6a693c6
starsbit <74101214+starsbit@users.noreply.github.com>
2026-04-10 12:47:49 +0200

    Bump version

--- frontend/src/environments/environment.e2e.ts
+++ frontend/src/environments/environment.e2e.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: '',
-  appVersion: '0.0.7',
+  appVersion: '0.0.8',
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '',
-  appVersion: '0.0.7',
+  appVersion: '0.0.8',
 };
